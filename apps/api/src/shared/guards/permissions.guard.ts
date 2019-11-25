2a78f03faa1d140adccc0ef38ee1586b9e42b097
Chau Tran <ctch5@mail.umsl.edu>
2019-11-25 08:28:51 -0600

    update guard

--- apps/api/src/shared/guards/permissions.guard.ts
+++ apps/api/src/shared/guards/permissions.guard.ts
@@ -3,6 +3,7 @@ import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { AuthUser } from '../../auth/auth-user';
 import { PERMISSIONS_DECORATOR } from '../decorators/permissions.decorator';
+import { PermissionPrivilege } from '../permission.enum';
 import { PermissionDictionary } from '../permission.types';
 
 @Injectable()
@@ -10,10 +11,9 @@ export class PermissionsGuard implements CanActivate {
   constructor(private readonly _reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const routePermissions = this._reflector.get<PermissionDictionary>(
-      PERMISSIONS_DECORATOR,
-      context.getHandler()
-    );
+    const routePermissions = this._reflector.get<
+      { [key in keyof PermissionDictionary]?: PermissionPrivilege }
+    >(PERMISSIONS_DECORATOR, context.getHandler());
 
     if (!routePermissions || !Object.keys(routePermissions).length) {
       return true;
